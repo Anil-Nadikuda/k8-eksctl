8e634ba990702af0e026b9141e94c3762a7b0196
Anil-Nadikuda <anilnadikuda1430@gmail.com>
2024-05-28 10:11:25 +0530

    changes

--- workstation.sh
+++ workstation.sh
@@ -64,3 +64,14 @@ VALIDATE $? "eksctl installation"
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 VALIDATE $? "kubens installation"
+
+#helm install
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+#k9s
+curl -sS https://webinstall.dev/k9s | bash
+
+#metrics server 
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
